169423f344a8d958136f058ec76238386ca5bccf
Denizbukut <denizbukut90@gmail.com>
2025-12-13 15:52:26 +0100

    goat bonus

--- components/draw-content.tsx
+++ components/draw-content.tsx
@@ -686,7 +686,7 @@ const [showInfo, setShowInfo] = useState(false)
         const goatPacks = cardType === "god" ? count : 0;
         
         if (goatPacks > 0) {
-          await incrementLegendaryDraw(user.username, goatPacks * 25);
+          await incrementLegendaryDraw(user.username, goatPacks * 75);
         }
         
         const ultimateCards = result.drawnCards?.filter((card: any) => card.rarity=== "ultimate") || []
